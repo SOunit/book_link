56d866149fd74a85caef67b9f06abe6c6c1c1671
SOunit <workbookwork7@gmail.com>
2021-08-09 19:40:04 -0700

    add node api

---
+++ api/index.js
@@ -0,0 +1,48 @@
+const keys = require('./keys');
+
+// Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres client setup
+const { Pool } = require('pg');
+const { response } = require('express');
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+});
+
+pgClient.on('connect', (client) => {
+  // initialize db by creating values table with number column
+  client
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.error(err));
+});
+
+// Express routes handlers
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * FROM values');
+  res.send(values.rows);
+});
+
+app.post('/values', (req, res) => {
+  const index = req.body.index;
+  pgClient.query('INSERT INTO values(number) VALUES ($1)', [index]);
+  res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+  console.log('Listening');
+});
