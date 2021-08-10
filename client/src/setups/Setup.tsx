2b68cf967ef3b4e4830fc83b7c0c7dcf09bb5834
SOunit <workbookwork7@gmail.com>
2021-08-10 03:37:25 -0700

    add axios data fetching

--- client/src/setups/Setup.tsx
+++ client/src/setups/Setup.tsx
@@ -1,11 +1,20 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Setup = () => {
   const [index, setIndex] = useState('');
   const [values, setValues] = useState<number[]>([]);
 
+  const fetchData = async () => {
+    const result = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    console.log(result.data);
+  };
+
   useEffect(() => {
     setValues([0, 1, 2]);
+    fetchData();
   }, []);
 
   return <div>Setup</div>;
