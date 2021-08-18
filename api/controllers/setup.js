94e9435dc47fd3a6eae494fcdd267a4accc941e0
SOunit <workbookwork7@gmail.com>
2021-08-18 13:47:08 -0700

    add model to setup value

--- api/controllers/setup.js
+++ api/controllers/setup.js
@@ -1,19 +1,11 @@
-// const setupController = require('../controllers/setup');
-const pgClient = require('../util/database');
-pgClient.on('connect', (client) => {
-  // initialize db by creating values table with number column
-  client
-    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((err) => console.error(err));
-});
+const Value = require('../models/value');
 
 exports.getHi = (req, res, next) => {
   res.send('hi');
 };
 
 exports.getValues = async (req, res) => {
-  const values = await pgClient.query('SELECT * FROM values');
-  // values.rows = [ { number: 5 }, { number: 7 }, { number: 3 } ]
+  const values = await Value.fetchAll();
   res.send(values.rows);
 };
 
@@ -27,6 +19,8 @@ exports.postValue = async (req, res) => {
   }
 
   // update db
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+  const value = new Value(index);
+  value.save();
+
   res.send({ working: true });
 };
