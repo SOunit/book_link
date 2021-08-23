1c490d82e8b759e9bbb1cd28965bd0b63388e25b
SOunit <workbookwork7@gmail.com>
2021-08-22 23:27:09 -0700

    change layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,22 @@ import Layout from './components/layout/Layout';
 import SearchUsers from './pages/SearchUsers';
 import PageNotFound from './pages/PageNotFound';
 import './App.css';
-import Setup from './setups/Setup';
 
 function App() {
   return (
-    <Setup />
-    // <Layout>
-    //   <Switch>
-    //     <Route path='/' exact>
-    //       <Redirect to='/search' />
-    //     </Route>
-    //     <Route path='/search'>
-    //       <SearchUsers />
-    //     </Route>
-    //     <Route path='*'>
-    //       <PageNotFound />
-    //     </Route>
-    //   </Switch>
-    // </Layout>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/search' />
+        </Route>
+        <Route path='/search'>
+          <SearchUsers />
+        </Route>
+        <Route path='*'>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
