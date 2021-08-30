c937c9a0fa93bb20988112748f0cfde2b7d4598e
SOunit <workbookwork7@gmail.com>
2021-08-29 21:52:16 -0700

    add user model

---
+++ api/src/models/sequelize/user.ts
@@ -0,0 +1,18 @@
+import Sequelize from 'sequelize';
+import sequelize from '../../util/database';
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  about: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+});
