ac253ec96fea4fd3e52c56c6746946bf6750fab9
SOunit <workbookwork7@gmail.com>
2021-09-06 17:47:09 -0700

    add text by auth status

--- client/src/components/googleAuth/GoogleAuth.tsx
+++ client/src/components/googleAuth/GoogleAuth.tsx
@@ -1,19 +1,40 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import keys from '../../util/keys';
 
 const GoogleAuth: FC = () => {
-  console.log(keys);
+  const [isSignedIn, setIsSignedIn] = useState<boolean | null>(null);
+  let auth: any;
+
+  const onAuthChange = () => {
+    setIsSignedIn(auth.isSignedIn.get());
+  };
 
   useEffect(() => {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId: keys.GOOGLE_CLIENT_ID,
-        scope: 'email',
-      });
+      window.gapi.client
+        .init({
+          clientId: keys.GOOGLE_CLIENT_ID,
+          scope: 'email',
+        })
+        .then(() => {
+          auth = window.gapi.auth2.getAuthInstance();
+          setIsSignedIn(auth.isSignedIn.get());
+          auth.isSignedIn.listen(onAuthChange);
+        });
     });
   }, []);
 
-  return <div>Google Auth</div>;
+  const renderAuthButton = () => {
+    if (isSignedIn === null) {
+      return <div>Loading...</div>;
+    } else if (isSignedIn) {
+      return <div>Signed in</div>;
+    } else if (!isSignedIn) {
+      return <div>Not signed in</div>;
+    }
+  };
+
+  return <div>{renderAuthButton()}</div>;
 };
 
 export default GoogleAuth;
