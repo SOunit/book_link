a535903056675412f2f70e09e62692bb7858c46e
SOunit <workbookwork7@gmail.com>
2021-09-09 14:19:11 -0700

    fetch user to create new user

--- client/src/store/auth-context.tsx
+++ client/src/store/auth-context.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { FC, useState } from 'react';
 import keys from '../util/keys';
 
@@ -24,10 +25,44 @@ export const AuthContextProvider: FC = (props) => {
     localStorage.removeItem(keys.TOKEN_KEY);
   };
 
+  const createNewUser = async (id: string) => {
+    console.log('create new user', id);
+  };
+
+  const fetchUser = async (id: string) => {
+    // check user
+    const graphqlQuery = {
+      query: `
+          query FetchUser($id: ID!){
+            getUserCount(id: $id)
+          }
+
+        `,
+      variables: {
+        id,
+      },
+    };
+
+    return axios({
+      url: keys.GRAPHQL_REQUEST_URL,
+      method: 'post',
+      data: graphqlQuery,
+    });
+  };
+
   const loginHandler = (token: string | null) => {
     setToken(token);
     if (token) {
       localStorage.setItem(keys.TOKEN_KEY, token);
+
+      // check if user exists
+      fetchUser(token).then((result) => {
+        const amount = result.data.data.getUserCount;
+
+        if (amount <= 0) {
+          createNewUser(token);
+        }
+      });
     }
   };
 
--- client/src/util/keys.ts
+++ client/src/util/keys.ts
@@ -5,6 +5,7 @@ const keys = {
   GOOGLE_CLIENT_ID:
     '299755795397-qj7sdbcr9uq581gmv2bdruhmtdfo6vaj.apps.googleusercontent.com',
   TOKEN_KEY: 'book_link_token',
+  GRAPHQL_REQUEST_URL: '/api/graphql',
 };
 
 export default keys;
