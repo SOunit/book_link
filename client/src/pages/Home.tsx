42c4a3012cb6eb9122414843b3f95dda6c4bf9e3
SOunit <workbookwork7@gmail.com>
2021-09-10 12:59:54 -0700

    refactor home page using use-user custom hook

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,63 +1,16 @@
-import axios from 'axios';
-import {
-  FC,
-  Fragment,
-  useEffect,
-  useState,
-  useContext,
-  useCallback,
-} from 'react';
+import { FC, Fragment } from 'react';
 import { useHistory } from 'react-router';
-import AuthContext from '../store/auth-context';
-import UserType from '../models/User';
 import UserInfo from '../components/userInfo/UserInfo';
 import UserItems from '../components/userItems/UserItems';
 import Buttons from '../components/ui/Buttons/Buttons';
 import Button, { ButtonTypes } from '../components/ui/Buttons/Button';
+import useUser from '../hooks/user-user';
 
 type HomeProps = {};
 
 const Home: FC<HomeProps> = (props) => {
-  // FIXME: refactor to custom hook
-  const [user, setUser] = useState<UserType>();
-  const authCtx = useContext(AuthContext);
+  const user = useUser();
   const history = useHistory();
-
-  const fetchUser = useCallback(async () => {
-    const graphqlQuery = {
-      query: `
-              query FetchUser($id: ID!){
-                user(id: $id){
-                  id
-                  name
-                  about
-                  imageUrl
-                  items{
-                    id
-                    title
-                    imageUrl
-                  }
-                }
-              }
-
-            `,
-      variables: {
-        id: authCtx.token,
-      },
-    };
-
-    const result = await axios({
-      url: '/api/graphql',
-      method: 'post',
-      data: graphqlQuery,
-    });
-
-    setUser(result.data.data.user);
-  }, [authCtx.token]);
-
-  useEffect(() => {
-    fetchUser();
-  }, [fetchUser]);
 
   const editProfileClickHandler = () => {
     history.push(`/users/edit`);
