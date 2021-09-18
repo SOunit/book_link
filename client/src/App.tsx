14c53f6e3e40bce9ae055b30fb1f9f94dc9c5c23
SOunit <workbookwork7@gmail.com>
2021-09-18 07:34:29 -0700

    refactor, add protected route component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { useContext } from 'react';
-import AuthContext from './store/auth-context';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import SearchUsers from './pages/SearchUsers';
 import PageNotFound from './pages/PageNotFound';
@@ -10,46 +8,27 @@ import Login from './pages/Login';
 import EditProfile from './pages/EditProfile';
 import EditUserItems from './pages/EditUserItems';
 import Followings from './pages/Followings';
+import ProtectedRoute from './components/router/ProtectedRoute';
 import './App.css';
 
 function App() {
-  const authCtx = useContext(AuthContext);
-  const isLoggedIn = authCtx.isLoggedIn;
-
   return (
     <Layout>
       <Switch>
+        {/* <ProtectedRoute component={() => <div>loggedIn</div>} path='/' exact /> */}
         <Route path='/login'>
           <Login />
         </Route>
-        <Route path='/' exact>
-          {isLoggedIn && <Redirect to='/search' />}
-          {!isLoggedIn && <Redirect to='/login' />}
-        </Route>
-        <Route path='/search'>
-          {isLoggedIn && <SearchUsers />}
-          {!isLoggedIn && <Redirect to='/login' />}
-        </Route>
-        <Route path='/users/edit'>
-          {isLoggedIn && <EditProfile />}
-          {!isLoggedIn && <Redirect to='/login' />}
-        </Route>
-        <Route path='/users/items/edit'>
-          {isLoggedIn && <EditUserItems />}
-          {!isLoggedIn && <Redirect to='/login' />}
-        </Route>
-        <Route path='/users/:userId/followings'>
-          {isLoggedIn && <Followings />}
-          {!isLoggedIn && <Redirect to='/login' />}
-        </Route>
-        <Route path='/users/:userId'>
-          {isLoggedIn && <UserDetail />}
-          {!isLoggedIn && <Redirect to='/login' />}
-        </Route>
-        <Route path='/home'>
-          {isLoggedIn && <Home />}
-          {!isLoggedIn && <Redirect to='/login' />}
-        </Route>
+        <ProtectedRoute component={SearchUsers} path='/' exact />
+        <ProtectedRoute component={SearchUsers} path='/search' />
+        <ProtectedRoute component={EditProfile} path='/users/edit' />
+        <ProtectedRoute component={EditUserItems} path='/users/items/edit' />
+        <ProtectedRoute
+          component={Followings}
+          path='/users/:userId/followings'
+        />
+        <ProtectedRoute component={UserDetail} path='/users/:userId' />
+        <ProtectedRoute component={Home} path='/home' />
         <Route path='*'>
           <PageNotFound />
         </Route>
