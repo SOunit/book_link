6423fd64f4eb3285c0222a8a6f2d427c7f498de1
SOunit <workbookwork7@gmail.com>
2022-02-22 19:30:09 -0800

    add image component

---
+++ client/src/components/atoms/image.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import classes from './image.module.css';
+
+type Props = {
+  src: string;
+  alt: string;
+  className: string;
+};
+
+const Image: FC<Props> = ({ src, alt, className }) => {
+  return (
+    <div className={`${classes['image-container']} ${className}`}>
+      <img className={classes['image']} src={src} alt={alt} />
+    </div>
+  );
+};
+
+export default Image;
