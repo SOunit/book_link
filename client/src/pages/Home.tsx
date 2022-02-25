9f2f33de07a924171c294d90012a68abba5ae523
SOunit <workbookwork7@gmail.com>
2022-02-24 21:25:38 -0800

    add actions

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,9 +2,9 @@ import { FC, Fragment, useContext } from 'react';
 import { useHistory } from 'react-router';
 import UserInfo from '../components/userInfo/UserInfo';
 import UserItems from '../components/userItems/UserItems';
-import Buttons from '../components/ui/Buttons/Buttons';
-import Button, { ButtonTypes } from '../components/ui/Buttons/Button';
 import AuthContext from '../store/auth-context';
+import IconTextButton from '../components/molecules/icon-text-button';
+import classes from './home.module.css';
 
 type HomeProps = {};
 
@@ -20,27 +20,22 @@ const Home: FC<HomeProps> = () => {
     history.push(`/users/items/edit`);
   };
 
-  let userInfo = null;
-  let userItems = null;
-  if (loginUser) {
-    userInfo = <UserInfo user={loginUser} />;
-    userItems = <UserItems items={loginUser.items} />;
-  }
-
   return (
     <Fragment>
-      {userInfo}
-      <Buttons>
-        <Button
-          buttonText="Edit Profile"
-          buttonType={ButtonTypes.NORMAL}
-          onButtonClick={editProfileClickHandler}></Button>
-        <Button
-          buttonText="Edit Items"
-          buttonType={ButtonTypes.NORMAL}
-          onButtonClick={editUserItemsClickHandler}></Button>
-      </Buttons>
-      {userItems}
+      {loginUser && <UserInfo user={loginUser} />}
+      <div className={classes['home__actions']}>
+        <IconTextButton
+          iconName="far fa-pen"
+          text="Edit Profile"
+          onClick={editProfileClickHandler}
+        />
+        <IconTextButton
+          iconName="far fa-books"
+          text="Edit Items"
+          onClick={editUserItemsClickHandler}
+        />
+      </div>
+      {loginUser && <UserItems items={loginUser.items} />}
     </Fragment>
   );
 };
