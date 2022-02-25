b896b5b82bac066c9d72057c23f7c02cef5cc4a3
SOunit <workbookwork7@gmail.com>
2022-02-25 11:33:26 -0800

    rename edit-user component

--- client/src/pages/edit-user.tsx
+++ client/src/pages/edit-user.tsx
@@ -1,22 +1,22 @@
 import { FC, Fragment, useContext } from 'react';
 import UserInfo from '../components/organisms/userInfo/UserInfo';
-import UserEditForm from '../components/organisms/edit-user/edit-user-form';
+import EditUserForm from '../components/organisms/edit-user/edit-user-form';
 import AuthContext from '../store/auth-context';
 
 const EditUser: FC = () => {
   const { loginUser } = useContext(AuthContext);
 
   let userInfo;
-  let userEditForm;
+  let editUserForm;
   if (loginUser) {
     userInfo = <UserInfo user={loginUser} />;
-    userEditForm = <UserEditForm user={loginUser} />;
+    editUserForm = <EditUserForm user={loginUser} />;
   }
 
   return (
     <Fragment>
       {userInfo}
-      {userEditForm}
+      {editUserForm}
     </Fragment>
   );
 };
