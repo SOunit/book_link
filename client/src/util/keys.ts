ee5a982db8ec721f8bb225068e33c85c6108e9a9
SOunit <workbookwork7@gmail.com>
2022-03-05 14:36:41 -0800

    fix keys

--- client/src/util/keys.ts
+++ client/src/util/keys.ts
@@ -1,8 +1,10 @@
 const keys = {
-  GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
-  GRAPHQL_REQUEST_URL: process.env.REACT_APP_GRAPHQL_REQUEST_URL,
-  AWS_S3_URL: process.env.REACT_APP_AWS_S3_URL,
+  GOOGLE_CLIENT_ID:
+    '299755795397-qj7sdbcr9uq581gmv2bdruhmtdfo6vaj.apps.googleusercontent.com',
+  TOKEN_KEY: 'book_link_token',
+  GRAPHQL_REQUEST_URL: '/api/graphql',
+  AWS_S3_URL:
+    'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/',
 };
 
 export default keys;
