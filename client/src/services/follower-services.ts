81ecd033eaef50554a824df665808f0740fa3ccc
SOunit <workbookwork7@gmail.com>
2022-03-05 16:22:22 -0800

    add follower update logic

--- client/src/services/follower-services.ts
+++ client/src/services/follower-services.ts
@@ -21,4 +21,40 @@ export const followerServices = {
       data: graphqlQuery,
     });
   },
+
+  deleteFollower: async (userId: string, followerUserId: string) => {
+    const graphqlQuery = {
+      query: `
+                  mutation DeleteFollower($userId: ID!, $followerUserId: ID!){
+                    deleteFollower(userId: $userId, followerUserId: $followerUserId)
+                  }
+                  `,
+      variables: {
+        userId,
+        followerUserId,
+      },
+    };
+
+    await API({
+      data: graphqlQuery,
+    });
+  },
+
+  createFollower: async (userId: string, followerUserId: string) => {
+    const graphqlQuery = {
+      query: `
+                  mutation CreateFollower($userId: ID!, $followerUserId: ID!){
+                    createFollower(userId: $userId, followerUserId: $followerUserId)
+                  }
+                  `,
+      variables: {
+        userId,
+        followerUserId,
+      },
+    };
+
+    return await API({
+      data: graphqlQuery,
+    });
+  },
 };
