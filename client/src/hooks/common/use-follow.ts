ca337630d0951194dc2aa91110eaeb2f56c63d4e
SOunit <workbookwork7@gmail.com>
2022-03-05 17:03:09 -0800

    fix follow logic

--- client/src/hooks/common/use-follow.ts
+++ client/src/hooks/common/use-follow.ts
@@ -1,14 +1,14 @@
 import { followerServices, followingServices } from '../../services';
 
 export const useFollow = () => {
-  const unFollowUser = (loginUserId: string, followingUserId: string) => {
-    followingServices.deleteFollowing(loginUserId, followingUserId);
-    followerServices.deleteFollower(loginUserId, followingUserId);
+  const unFollowUser = (loginUserId: string, unFollowingUserId: string) => {
+    followingServices.deleteFollowing(loginUserId, unFollowingUserId);
+    followerServices.deleteFollower(unFollowingUserId, loginUserId);
   };
 
   const followUser = (loginUserId: string, followingUserId: string) => {
     followingServices.createFollowing(loginUserId, followingUserId);
-    followerServices.createFollower(loginUserId, followingUserId);
+    followerServices.createFollower(followingUserId, loginUserId);
   };
 
   return {
