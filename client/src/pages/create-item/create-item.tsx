1765228bc87a39c954efd67828d766487b22afdc
SOunit <workbookwork7@gmail.com>
2022-03-09 19:55:00 -0800

    add validator to create-item

--- client/src/pages/create-item/create-item.tsx
+++ client/src/pages/create-item/create-item.tsx
@@ -16,46 +16,72 @@ import {
 import { useAwsS3 } from '../../hooks/';
 import { itemServices } from '../../services';
 import { AuthContext } from '../../store';
+import { validate, VALIDATOR_REQUIRE } from '../../util';
 import classes from './create-item.module.css';
 
 type Inputs = {
-  title: string;
-  author: string;
+  title: { value: string; isValid: boolean };
+  author: { value: string; isValid: boolean };
+  isValid: boolean;
 };
 
 export const CreateItem: FC = () => {
   const [inputs, setInputs] = useState<Inputs>({
-    title: '',
-    author: '',
+    title: { value: '', isValid: false },
+    author: { value: '', isValid: false },
+    isValid: false,
   });
   const [imageFile, setImageFile] = useState<File>();
-  const { title, author } = inputs;
   const history = useHistory();
   const { loginUser, setLoginUser } = useContext(AuthContext);
   const { uploadImageToS3 } = useAwsS3();
 
+  const { title, author } = inputs;
+
   const changeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputs((prevState) => ({ ...prevState, title: event.target.value }));
+    const inputTitle = event.target.value;
+    const titleIsValid = validate(inputTitle, [VALIDATOR_REQUIRE()]);
+    const formIsValid = true;
+
+    setInputs((prevState) => ({
+      ...prevState,
+      title: {
+        value: inputTitle,
+        isValid: titleIsValid,
+      },
+      isValid: titleIsValid && formIsValid,
+    }));
   };
   const changeAuthorHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputs((prevState) => ({ ...prevState, author: event.target.value }));
+    const inputAuthor = event.target.value;
+    setInputs((prevState) => ({
+      ...prevState,
+      author: {
+        value: inputAuthor,
+        isValid: validate(inputAuthor, [VALIDATOR_REQUIRE()]),
+      },
+    }));
   };
   const submitHandler = async (event: SyntheticEvent) => {
+    event.preventDefault();
+
     if (!loginUser || !loginUser.items || !imageFile) {
       return;
     }
 
-    event.preventDefault();
-
     try {
       // upload image
-      const imageUrl = await uploadImageToS3(imageFile);
+      const imageUrl = (await uploadImageToS3(imageFile)) || '';
       if (!imageUrl) {
         return;
       }
 
       // update db
-      const itemRes = await itemServices.createItem(title, author, imageUrl);
+      const itemRes = await itemServices.createItem(
+        title.value,
+        author.value,
+        imageUrl,
+      );
       const newItem = itemRes.data.data.createItem;
       await itemServices.addUserItem(loginUser.id, newItem.id);
 
@@ -75,14 +101,14 @@ export const CreateItem: FC = () => {
       <form className={classes['create-item__form']} onSubmit={submitHandler}>
         <ValidateInput
           placeholder="Title"
-          initialValue={title}
+          initialValue={title.value}
           onChange={changeTitleHandler}
           className={classes['create-item__form-input']}
           errorMessage="Please put valid title."
         />
         <ValidateInput
           placeholder="Author"
-          initialValue={author}
+          initialValue={author.value}
           onChange={changeAuthorHandler}
           className={classes['create-item__form-input']}
           errorMessage="Please put valid Author."
