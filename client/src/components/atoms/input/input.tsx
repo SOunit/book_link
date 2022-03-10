7f30c40520fa1db9b0767ba7ff79b56a06be299d
SOunit <workbookwork7@gmail.com>
2022-03-09 18:14:57 -0800

    fix input component

--- client/src/components/atoms/input/input.tsx
+++ client/src/components/atoms/input/input.tsx
@@ -1,5 +1,4 @@
-import { ChangeEvent, FC, useState } from 'react';
-import { validate, VALIDATOR_REQUIRE } from '../../../util';
+import { FC } from 'react';
 import classes from './input.module.scss';
 
 type Props = {
@@ -8,7 +7,6 @@ type Props = {
   type?: string;
   placeholder?: string;
   className?: string;
-  errorMessage?: string;
 };
 
 export const Input: FC<Props> = ({
@@ -17,39 +15,14 @@ export const Input: FC<Props> = ({
   placeholder,
   className,
   onChange,
-  errorMessage,
 }) => {
-  const [inputState, setInputState] = useState({
-    value: initialValue || '',
-    isValid: true,
-  });
-
-  const { value, isValid } = inputState;
-
-  const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    // update parent state
-    onChange(event);
-
-    // update component state
-    setInputState((prevState) => ({
-      ...prevState,
-      value: event.target.value,
-      isValid: validate(event.target.value, [VALIDATOR_REQUIRE()]),
-    }));
-  };
-
   return (
-    <div className={`${classes['input-wrapper']} ${className}`}>
-      <input
-        placeholder={placeholder}
-        type={type}
-        value={value}
-        onChange={inputChangeHandler}
-        className={`${classes['input']} ${
-          !isValid && classes['input--invalid']
-        }`}
-      />
-      {!isValid && <p className={classes['error-message']}>{errorMessage}</p>}
-    </div>
+    <input
+      placeholder={placeholder}
+      type={type}
+      value={initialValue}
+      onChange={onChange}
+      className={`${classes['input']} ${className}`}
+    />
   );
 };
