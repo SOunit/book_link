becde224721e2a4df68c54ff1ae2ddf8d8639bfc
SOunit <workbookwork7@gmail.com>
2022-03-16 20:22:44 -0700

    add style to follow-number component

--- client/src/components/molecules/follow-number/follow-number.tsx
+++ client/src/components/molecules/follow-number/follow-number.tsx
@@ -1,16 +1,23 @@
-import { FC, Fragment } from 'react';
+import { FC } from 'react';
 import classes from './follow-number.module.scss';
 
 type Props = {
-  followCount: number;
+  followCount?: number;
   unitTitle: string;
+  className?: string;
 };
 
-export const FollowNumber: FC<Props> = ({ followCount, unitTitle }) => {
+export const FollowNumber: FC<Props> = ({
+  followCount,
+  unitTitle,
+  className,
+}) => {
   return (
-    <Fragment>
-      <span className={classes['follow-number']}>{followCount}</span>{' '}
+    <p className={`${classes['follow-number']} ${className}`}>
+      <span className={classes['follow-number__number']}>
+        {followCount ? followCount : 0}
+      </span>{' '}
       {unitTitle}
-    </Fragment>
+    </p>
   );
 };
