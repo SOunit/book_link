8da7f296d14fe4c0114bb5075cf33a6833455220
SOunit <workbookwork7@gmail.com>
2022-03-29 15:20:06 -0700

    fix chat-list page logic

--- client/src/presentation/pages/chat-list/chat-list.tsx
+++ client/src/presentation/pages/chat-list/chat-list.tsx
@@ -1,42 +1,20 @@
-import { FC, useEffect, useState } from 'react';
-import { Chat } from '../../../domain';
-import { useChatAdapter, useAuthStorage } from '../../../services';
-import { NotFoundMessage, Spinner } from '../../components/molecules';
+import { FC } from 'react';
+import { useChatStorage } from '../../../services';
+import { NotFoundMessage } from '../../components/molecules';
 import { ChatList as ChatLogList } from '../../components/organisms';
-import classes from './chat-list.module.scss';
 
 export const ChatList: FC = () => {
-  const { loginUser } = useAuthStorage();
-  const [chats, setChats] = useState<Chat[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const chatAdapter = useChatAdapter();
-
-  useEffect(() => {
-    if (loginUser) {
-      chatAdapter
-        .fetchChatList(loginUser.id)
-        .then((res) => {
-          const chats = res.data.data.getUserChatList;
-          setChats(chats);
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          console.log(err);
-          setIsLoading(false);
-        });
-    }
-  }, [loginUser]);
+  const { chatList } = useChatStorage();
 
   return (
     <div>
-      {isLoading && <Spinner className={classes['spinner']} />}
-      {!isLoading && chats && chats.length === 0 && (
+      {(!chatList || (chatList && chatList.length === 0)) && (
         <NotFoundMessage
           title="No Chat Found"
           text="Let's chat with somebody."
         />
       )}
-      {!isLoading && chats && <ChatLogList chatList={chats} />}
+      {chatList && <ChatLogList chatList={chatList} />}
     </div>
   );
 };
