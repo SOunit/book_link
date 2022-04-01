df7fd6a800bf7cc331121b313d8a468cd5a77b4b
SOunit <workbookwork7@gmail.com>
2022-04-01 15:08:26 -0700

    fix pg error

---
+++ api/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+const { IgnorePlugin } = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  target: 'node',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin(),
+    new IgnorePlugin({
+      resourceRegExp: /^pg-native$/,
+    }),
+  ],
+};
