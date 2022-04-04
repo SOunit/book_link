ed82facce2b62e8926a3751f08f5260ef95591a6
SOunit <workbookwork7@gmail.com>
2022-04-04 16:56:38 -0700

    fix test data create logic

--- api/src/data/index.ts
+++ api/src/data/index.ts
@@ -4,33 +4,9 @@ import { createItems } from './create-items';
 import { createUsers } from './create-users';
 
 export const createTestData = async () => {
-  const {
-    jack,
-    kate,
-    kevin,
-    rebecca,
-    user5,
-    user6,
-    user7,
-    user8,
-    user9,
-    user10,
-    user11,
-  } = await createUsers();
+  await createUsers();
 
-  await createItems(
-    jack,
-    rebecca,
-    kevin,
-    kate,
-    user5,
-    user6,
-    user7,
-    user8,
-    user9,
-    user10,
-    user11,
-  );
+  await createItems();
 
   await createChat();
 
