7c4db9707434c309b25f2d0c10c589d5422de88d
SOunit <workbookwork7@gmail.com>
2022-04-08 16:33:53 -0700

    fix image data in s3

--- api/src/data/create-users.ts
+++ api/src/data/create-users.ts
@@ -7,7 +7,7 @@ export const createUsers = async () => {
     name: 'Jack',
     about: `I am a test user! You can edit whatever you want, but All changes will be gone. because every deploy create new records!`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/vicky-hladynets-C8Ta0gwPbQg-unsplash.jpg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/vicky-hladynets-C8Ta0gwPbQg-unsplash.jpeg',
   });
 
   await User.create({
@@ -18,7 +18,7 @@ export const createUsers = async () => {
               repudiandae molestiae voluptate? Minima tenetur quos rem nulla quo
               praesentium libero reiciendis quam!`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/houcine-ncib-B4TjXnI0Y2c-unsplash.jpg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/yunming-wang-G9f4Enb8XVM-unsplash.jpeg',
   });
 
   await User.create({
@@ -26,7 +26,7 @@ export const createUsers = async () => {
     name: 'Kevin',
     about: ``,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/craig-mckay-jmURdhtm7Ng-unsplash.jpg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/troy-wade-ncfTHzTjtCw-unsplash.jpeg',
   });
 
   await User.create({
@@ -37,7 +37,7 @@ export const createUsers = async () => {
               animi quas! Temporibus error harum nam sapiente totam maiores non
               vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/troy-wade-ncfTHzTjtCw-unsplash.jpg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/stephanie-liverani-Zz5LQe-VSMY-unsplash.jpeg',
   });
 
   await User.create({
@@ -48,7 +48,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/karina-tess-l35dDPD3Gys-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/roksolana-zasiadko-LyeduBb2Auk-unsplash.jpeg',
   });
 
   await User.create({
@@ -59,7 +59,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/jeffery-erhunse-Z9lbmEjyYjU-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/ransford-quaye-DzAFv1iVMGg-unsplash.jpeg',
   });
 
   await User.create({
@@ -70,7 +70,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/irene-strong-v2aKnjMbP_k-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/raj-rana-15Vb4B_ma_s-unsplash.jpeg',
   });
 
   await User.create({
@@ -81,7 +81,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/ionut-comanici-RDcEWH5hSDE-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/prince-akachi-LWkFHEGpleE-unsplash.jpeg',
   });
 
   await User.create({
@@ -92,7 +92,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/gabriel-silverio-K_b41GaWC5Y-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/philip-martin-5aGUyCW_PJw-unsplash.jpeg',
   });
 
   await User.create({
@@ -103,7 +103,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/ehsan-ahmadi-vsWy6nchcOs-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/nishanth-avva-SdCaK9YKdwk-unsplash.jpeg',
   });
 
   await User.create({
@@ -114,7 +114,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/dave-goudreau-MJ2zd_OfxSw-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/michael-dam-mEZ3PoFGs_k-unsplash.jpeg',
   });
 
   await User.create({
@@ -125,7 +125,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/clive-surreal-9kQBQqY_xrk-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/leilani-angel-K84vnnzxmTQ-unsplash.jpeg',
   });
 
   await User.create({
@@ -136,7 +136,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/karina-tess-l35dDPD3Gys-unsplash.jpeg',
   });
 
   await User.create({
@@ -147,7 +147,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/christian-buehner-DItYlc26zVI-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/jeffery-erhunse-Z9lbmEjyYjU-unsplash.jpeg',
   });
 
   await User.create({
@@ -158,7 +158,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/leilani-angel-K84vnnzxmTQ-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/irene-strong-v2aKnjMbP_k-unsplash.jpeg',
   });
 
   await User.create({
@@ -169,7 +169,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/michael-dam-mEZ3PoFGs_k-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/ionut-comanici-RDcEWH5hSDE-unsplash.jpeg',
   });
 
   await User.create({
@@ -180,7 +180,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/nishanth-avva-SdCaK9YKdwk-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/houcine-ncib-B4TjXnI0Y2c-unsplash.jpeg',
   });
 
   await User.create({
@@ -191,7 +191,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/philip-martin-5aGUyCW_PJw-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/gabriel-silverio-K_b41GaWC5Y-unsplash.jpeg',
   });
 
   await User.create({
@@ -202,7 +202,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/prince-akachi-LWkFHEGpleE-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/ehsan-ahmadi-vsWy6nchcOs-unsplash.jpeg',
   });
 
   await User.create({
@@ -213,7 +213,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/raj-rana-15Vb4B_ma_s-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/dave-goudreau-MJ2zd_OfxSw-unsplash.jpeg',
   });
 
   await User.create({
@@ -224,7 +224,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/ransford-quaye-DzAFv1iVMGg-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/craig-mckay-jmURdhtm7Ng-unsplash.jpeg',
   });
 
   await User.create({
@@ -235,7 +235,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/roksolana-zasiadko-LyeduBb2Auk-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/clive-surreal-9kQBQqY_xrk-unsplash.jpeg',
   });
 
   await User.create({
@@ -246,7 +246,7 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/stephanie-liverani-Zz5LQe-VSMY-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.jpeg',
   });
 
   await User.create({
@@ -257,6 +257,6 @@ export const createUsers = async () => {
     animi quas! Temporibus error harum nam sapiente totam maiores non
     vitae impedit recusandae aut.`,
     imageUrl:
-      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/yunming-wang-G9f4Enb8XVM-unsplash.jpeg',
+      'https://image-upload-sample-blog-app-123.s3.ca-central-1.amazonaws.com/test_user_id/christian-buehner-DItYlc26zVI-unsplash.jpeg',
   });
 };
