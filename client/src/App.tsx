e76f6fc40714ef9a03cbbadaf04a8e424cbd8496
SOunit <workbookwork7@gmail.com>
2022-04-09 13:03:16 -0700

    add lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,56 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { Layout } from './presentation/components/organisms';
 import { ProtectedRoute } from './presentation/components/molecules';
-import {
-  SearchUsers,
-  PageNotFound,
-  UserDetail,
-  Home,
-  Login,
-  EditUser,
-  EditUserItems,
-  Chat,
-  ChatList,
-  CreateItem,
-  Follow,
-} from './presentation/pages';
+import { SearchUsers, Login } from './presentation/pages';
 import { useAuthStorage, useSocketAdapter } from './services';
 import './App.css';
+
+const PageNotFound = lazy(() =>
+  import('./presentation/pages').then(({ PageNotFound }) => ({
+    default: PageNotFound,
+  })),
+);
+const UserDetail = lazy(() =>
+  import('./presentation/pages').then(({ UserDetail }) => ({
+    default: UserDetail,
+  })),
+);
+const Home = lazy(() =>
+  import('./presentation/pages').then(({ Home }) => ({
+    default: Home,
+  })),
+);
+const EditUser = lazy(() =>
+  import('./presentation/pages').then(({ EditUser }) => ({
+    default: EditUser,
+  })),
+);
+const EditUserItems = lazy(() =>
+  import('./presentation/pages').then(({ EditUserItems }) => ({
+    default: EditUserItems,
+  })),
+);
+const Chat = lazy(() =>
+  import('./presentation/pages').then(({ Chat }) => ({
+    default: Chat,
+  })),
+);
+const ChatList = lazy(() =>
+  import('./presentation/pages').then(({ ChatList }) => ({
+    default: ChatList,
+  })),
+);
+const CreateItem = lazy(() =>
+  import('./presentation/pages').then(({ CreateItem }) => ({
+    default: CreateItem,
+  })),
+);
+const Follow = lazy(() =>
+  import('./presentation/pages').then(({ Follow }) => ({
+    default: Follow,
+  })),
+);
 
 function App() {
   const { socket } = useSocketAdapter();
@@ -35,40 +69,42 @@ function App() {
 
   return (
     <Layout>
-      <Switch>
-        <Route path="/login">
-          {authStorage.isLoggedIn ? <Redirect to="/" /> : <Login />}
-        </Route>
-        {/* Search */}
-        <ProtectedRoute component={SearchUsers} path="/" exact />
-        <ProtectedRoute component={SearchUsers} path="/search" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/login">
+            {authStorage.isLoggedIn ? <Redirect to="/" /> : <Login />}
+          </Route>
+          {/* Search */}
+          <ProtectedRoute component={SearchUsers} path="/" exact />
+          <ProtectedRoute component={SearchUsers} path="/search" />
 
-        {/* Users */}
-        <ProtectedRoute component={EditUser} path="/users/edit" />
-        <ProtectedRoute component={EditUserItems} path="/users/items/edit" />
-        <ProtectedRoute component={Follow} path="/users/:userId/followings" />
-        <ProtectedRoute component={Follow} path="/users/:userId/followers" />
-        <ProtectedRoute component={UserDetail} path="/users/:userId" />
+          {/* Users */}
+          <ProtectedRoute component={EditUser} path="/users/edit" />
+          <ProtectedRoute component={EditUserItems} path="/users/items/edit" />
+          <ProtectedRoute component={Follow} path="/users/:userId/followings" />
+          <ProtectedRoute component={Follow} path="/users/:userId/followers" />
+          <ProtectedRoute component={UserDetail} path="/users/:userId" />
 
-        {/* Home */}
-        <ProtectedRoute component={Home} path="/home" />
+          {/* Home */}
+          <ProtectedRoute component={Home} path="/home" />
 
-        {/* Items */}
-        <ProtectedRoute component={CreateItem} path="/items/new" />
+          {/* Items */}
+          <ProtectedRoute component={CreateItem} path="/items/new" />
 
-        {/* Chats */}
-        <ProtectedRoute component={ChatList} path="/chats" exact />
-        <ProtectedRoute
-          component={Chat}
-          path="/chats/:userId"
-          socket={socket}
-        />
+          {/* Chats */}
+          <ProtectedRoute component={ChatList} path="/chats" exact />
+          <ProtectedRoute
+            component={Chat}
+            path="/chats/:userId"
+            socket={socket}
+          />
 
-        {/* Page Not Found */}
-        <Route path="*">
-          <PageNotFound />
-        </Route>
-      </Switch>
+          {/* Page Not Found */}
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
